d6aaee4b79d51474934a5cb8235948a64d83b6a3
Omar <omarelghamry@icloud.com>
2022-08-08 23:47:06 +0400

    cleaned files

--- my_config.py
+++ my_config.py
@@ -6,7 +6,7 @@
 #    By: ommohame < ommohame@student.42abudhabi.ae> +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/04 00:44:05 by ommohame          #+#    #+#              #
-#    Updated: 2022/08/08 23:39:40 by ommohame         ###   ########.fr        #
+#    Updated: 2022/08/08 23:46:25 by ommohame         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,23 +14,23 @@
 labs = [1]
 
 # the rows in each lab
-rows = [[1, 3]]
+rows = [[1, 4]]
 
 # the seats in each row
-seats = [1, 20]
+seats = [1, 14]
 
 # exam id on the api
 exam_id = 10949
 
 # exam get call
-get_exam = f"https://api.intra.42.fr/v2/events/{exam_id}/events_users"
+get_exam = f"https://api.intra.42.fr/v2/exams/{exam_id}/exams_users"
 
 # exam type
 exam = "cursus"
 # exam = "piscine"
 
 # email settings
-email_host = 'smtp.mail.me.com'
+email_host = ''
 email_port = 587
-email_id = 'omarelghamry@icloud.com'
-email_pass = 'bpye-ylwg-aqmq-fokt'
+email_id = ''
+email_pass = ''
